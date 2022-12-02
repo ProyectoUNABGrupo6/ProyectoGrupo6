93de2795d97af1c96cfcfdef3e9f42d0378fdcb4
karl065 <karlos065@hotmail.com>
2022-12-02 01:44:56 -0500

    Modificacion a Ferreteria

--- app/src/main/java/grupo6/proyectogrupo6/Adapters/ProductoAdapters.java
+++ app/src/main/java/grupo6/proyectogrupo6/Adapters/ProductoAdapters.java
@@ -1,16 +1,20 @@
 package grupo6.proyectogrupo6.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import grupo6.proyectogrupo6.Entities.Producto;
+import grupo6.proyectogrupo6.Informacion;
+import grupo6.proyectogrupo6.Productos;
 import grupo6.proyectogrupo6.R;
 
 public class ProductoAdapters extends BaseAdapter {
@@ -43,11 +47,11 @@ public class ProductoAdapters extends BaseAdapter {
 
         View view;
         LayoutInflater layoutInflater = LayoutInflater.from(this.context);
-        view = layoutInflater.inflate(R.layout.produtos_template,null);
+        view = layoutInflater.inflate(R.layout.produtos_template, null);
 
         Producto producto = arrayList.get(position);
 
-        ImageView imgProductoTemplate = view.findViewById(R.id.imgProductoTemplate);
+        ImageButton imgProductoTemplate = view.findViewById(R.id.imgProductoTemplate);
         TextView txtProductoTituloTemplate = view.findViewById(R.id.txtProductoTituloTemplate);
         TextView txtDescripcionTemplate = view.findViewById(R.id.txtDescripcionTemplate);
         TextView txtPrecioTemplate = view.findViewById(R.id.txtPrecioTemplate);
@@ -57,6 +61,19 @@ public class ProductoAdapters extends BaseAdapter {
         txtDescripcionTemplate.setText(producto.getDescripcion());
         txtPrecioTemplate.setText(String.valueOf(producto.getPrecio()));
 
+        imgProductoTemplate.setOnClickListener(View -> {
+            Intent intent = new Intent(context, Informacion.class);
+
+            intent.putExtra("imageTitulo", R.drawable.ferresix);
+            intent.putExtra("imageCarrito", R.drawable.carrito);
+            intent.putExtra("titulo", producto.getName());
+            intent.putExtra("descripcion", producto.getDescripcion());
+            intent.putExtra("imageCode", producto.getImagen());
+            intent.putExtra("precio", producto.getPrecio());
+            context.startActivity(intent);
+        });
+
         return view;
     }
+
 }

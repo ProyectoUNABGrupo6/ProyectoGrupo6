3bc705f7d3a0c4f3ba08d1c081c19146159b19dd
karl065 <karlos065@hotmail.com>
2022-12-01 21:06:03 -0500

    Modificacion a Ferreteria

--- app/src/main/java/grupo6/proyectogrupo6/MainActivity.java
+++ app/src/main/java/grupo6/proyectogrupo6/MainActivity.java
@@ -1,44 +1,70 @@
 package grupo6.proyectogrupo6;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText usuario;
-    private EditText contrasena;
-    public Button botonLogin;
+    public ImageButton imgMaterial;
+    public ImageButton imgHerramientas;
+    public ImageButton imgElectricos;
+    private TextView txtMaterial;
+    private TextView txtHerramienta;
+    private TextView txtElectrico;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usuario = findViewById(R.id.insUsuario);
-        contrasena = findViewById(R.id.insContrasena);
-        botonLogin = findViewById(R.id.botonLogin);
+        imgMaterial = findViewById(R.id.imgMaterial);
+        imgHerramientas = findViewById(R.id.imgHerramienta);
+        imgElectricos = findViewById(R.id.imgCables);
+        txtMaterial = findViewById(R.id.txtMate);
+        txtHerramienta = findViewById(R.id.txtHerram);
+        txtElectrico = findViewById(R.id.txtElect);
 
-        botonLogin.setOnClickListener(this::acceso);
+        txtMaterial.setOnClickListener(View -> {
+            Intent intent = new Intent(getApplicationContext(), Productos.class);
+            startActivity(intent);
 
+        });
 
+        txtHerramienta.setOnClickListener(View -> {
+            Intent intent = new Intent(getApplicationContext(), Productos.class);
+            startActivity(intent);
+
+        });
+
+        txtElectrico.setOnClickListener(View -> {
+            Intent intent = new Intent(getApplicationContext(), Productos.class);
+            startActivity(intent);
+
+        });
+        
+        imgMaterial.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Productos.class);
+            startActivity(intent);
+
+        });
+
+        imgHerramientas.setOnClickListener(View -> {
+            Intent intent = new Intent(getApplicationContext(), Productos.class);
+            startActivity(intent);
+
+        });
+
+        imgElectricos.setOnClickListener(View -> {
+
+            Intent intent = new Intent(getApplicationContext(), Productos.class);
+            startActivity(intent);
+        });
     }
 
-    public void acceso(View view){
-            String user = usuario.getText().toString();
-            String pass = contrasena.getText().toString();
-
-            if(user.equals("Grupo6") && pass.equals("Grupo6")){
-                Toast.makeText(this,"Bievenido",Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(getApplicationContext(), Productos.class);
-                startActivity(intent);
-            }else {
-                Toast.makeText(this,"Usuario o contraseña invalidos", Toast.LENGTH_LONG).show();
-            }
-    }
 }
--- app/src/main/java/grupo6/proyectogrupo6/Productos.java
+++ app/src/main/java/grupo6/proyectogrupo6/Productos.java
@@ -1,70 +1,29 @@
 package grupo6.proyectogrupo6;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Productos extends AppCompatActivity {
 
-    public Button producto1;
-    public Button producto2;
-    public Button producto3;
-    public Button cerrar;
+    public ImageButton botonAtras;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
 
-        producto1 = findViewById(R.id.botonProducto1);
-        producto2 = findViewById(R.id.botonProducto2);
-        producto3 = findViewById(R.id.botonProducto3);
-        cerrar = findViewById(R.id.botonCerrar);
+        botonAtras = findViewById(R.id.imgAtras);
 
-        producto1.setOnClickListener(view -> {
-            Intent intent = new Intent(Productos.this, Informacion.class);
-            String cantidad = "10.000";
-            String precio = "$1.000";
-            intent.putExtra("image", R.drawable.fresa);
-            intent.putExtra("pre", precio);
-            intent.putExtra("cant", cantidad);
-
-            startActivity(intent);
-        });
-        producto2.setOnClickListener(view -> {
-            Intent intent = new Intent(Productos.this, Informacion.class);
-            String cantidad = "5.000";
-            String precio = "$2.000";
-            intent.putExtra("image", R.drawable.mango);
-            intent.putExtra("pre", precio);
-            intent.putExtra("cant", cantidad);
-
-            startActivity(intent);
-        });
-        producto3.setOnClickListener(view -> {
-            Intent intent = new Intent(Productos.this, Informacion.class);
-            String cantidad = "50.000";
-            String precio = "$500";
-            intent.putExtra("image", R.drawable.frutita);
-            intent.putExtra("pre", precio);
-            intent.putExtra("cant", cantidad);
-
+        botonAtras.setOnClickListener(View ->{
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         });
 
-        cerrar.setOnClickListener(this::cerrarSesion);
 
-
-    }
-
-
-    public void cerrarSesion(View view) {
-        Toast.makeText(this, "Sesion Cerrada", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
     }
 }

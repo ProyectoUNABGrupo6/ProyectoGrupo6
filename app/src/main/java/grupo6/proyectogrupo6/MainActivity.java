bf6fde718842a5885ba96d0e44541c57be5d6c80
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-03 16:29:12 -0500

    base navigation view

--- app/src/main/java/grupo6/proyectogrupo6/MainActivity.java
+++ app/src/main/java/grupo6/proyectogrupo6/MainActivity.java
@@ -1,8 +1,11 @@
 package grupo6.proyectogrupo6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout dl = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imgMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dl.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }

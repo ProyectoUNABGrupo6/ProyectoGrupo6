e517de3a5469b35dca6688bce33fd7b186914864
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-03 20:23:17 -0500

    UI_Menu Header and Item

--- app/src/main/java/grupo6/proyectogrupo6/MainActivity.java
+++ app/src/main/java/grupo6/proyectogrupo6/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,12 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initMenu();
+
+    }
+
+    private void initMenu(){
         final DrawerLayout dl = findViewById(R.id.drawerLayout);
+        final NavigationView navigationView = findViewById(R.id.navigationView);
+
         findViewById(R.id.imgMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dl.openDrawer(GravityCompat.START);
             }
         });
+        navigationView.setItemIconTintList(null);
     }
 }

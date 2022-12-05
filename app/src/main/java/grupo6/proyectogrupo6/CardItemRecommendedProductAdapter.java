09e41567e6b3dbe411defe711822782c77e03d95
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-05 18:40:39 -0500

    UI_recommendedProduct add recommended product adapter

---
+++ app/src/main/java/grupo6/proyectogrupo6/CardItemRecommendedProductAdapter.java
@@ -0,0 +1,62 @@
+package grupo6.proyectogrupo6;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardItemRecommendedProductAdapter extends RecyclerView.Adapter<CardItemRecommendedProductAdapter.ViewHolder>{
+
+    private List<CardItemRecommendedProductModel> list = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_card_item_recommended_product,null,false);
+        return new CardItemRecommendedProductAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.updateItem(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public void updateList(List<CardItemRecommendedProductModel> list) {
+        this.list.clear();
+        this.list = list;
+        notifyDataSetChanged();
+    }
+    
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imgCategory;
+        TextView nameCategory;
+        TextView detailsCategory;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgCategory = itemView.findViewById(R.id.cardItemRecommendedProductImage);
+            nameCategory = itemView.findViewById(R.id.cardItemRecommendedProductName);
+            detailsCategory = itemView.findViewById(R.id.cardItemRecommendedProductDetails);
+        }
+
+        public void updateItem(CardItemRecommendedProductModel cardItemRecommendedProductModel) {
+            imgCategory.setImageResource(cardItemRecommendedProductModel.getImage());
+            nameCategory.setText(cardItemRecommendedProductModel.getName());
+            detailsCategory.setText(cardItemRecommendedProductModel.getDetails());
+        }
+    }
+
+}

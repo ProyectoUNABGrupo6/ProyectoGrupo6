0e5e600b0675ab8f5518e86c2d7cf298eb7b73f9
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-04 20:07:54 -0500

    UI_imageSlider add build

--- app/src/main/java/grupo6/proyectogrupo6/MenuItemHomeFragment.java
+++ app/src/main/java/grupo6/proyectogrupo6/MenuItemHomeFragment.java
@@ -1,58 +1,33 @@
 package grupo6.proyectogrupo6;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MenuItemHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuItemHomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MenuItemHomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment menuHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenuItemHomeFragment newInstance(String param1, String param2) {
-        MenuItemHomeFragment fragment = new MenuItemHomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private SliderView svm;
+    private MenuItemHomeSliderAdapter misa;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +35,37 @@ public class MenuItemHomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu_item_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        initAdapter();
+        loadData();
+    }
+
+    private void init(View v){
+        svm = v.findViewById(R.id.menuItemHomeSliderImage);
+    }
+    private void initAdapter() {
+        misa = new MenuItemHomeSliderAdapter(getContext());
+        svm.setSliderAdapter(misa);
+        svm.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using IndicatorAnimationType. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        svm.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        svm.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        svm.setIndicatorSelectedColor(Color.WHITE);
+        svm.setIndicatorUnselectedColor(Color.GRAY);
+        svm.setScrollTimeInSec(4); //set scroll delay in seconds :
+        svm.startAutoCycle();
+    }
+    private void loadData() {
+
+        List<SliderItemImageModelView> lista = new ArrayList<>();
+        lista.add(new SliderItemImageModelView(R.drawable.img_logo, "Imagen 1"));
+        lista.add(new SliderItemImageModelView(R.drawable.img_logo, "Imagen 2"));
+        lista.add(new SliderItemImageModelView(R.drawable.img_logo, "Imagen 3"));
+        lista.add(new SliderItemImageModelView(R.drawable.img_logo, "Imagen 4"));
+        misa.updateItem(lista);
     }
 }

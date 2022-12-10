9ad8a8ace20268cac85e10038c24eb8454be0d27
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-10 09:50:07 -0500

    UI_adminCategory select photo

--- app/src/main/java/grupo6/proyectogrupo6/MenuItemCategoryAddFragment.java
+++ app/src/main/java/grupo6/proyectogrupo6/MenuItemCategoryAddFragment.java
@@ -1,15 +1,32 @@
 package grupo6.proyectogrupo6;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class MenuItemCategoryAddFragment extends Fragment {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.jetbrains.annotations.NotNull;
+
+public class MenuItemCategoryAddFragment extends Fragment
+                                         implements View.OnClickListener{
+
+    //add photo
+    private FloatingActionButton addPhotoButton;
+    private ImageView addImg;
+    private ActivityResultLauncher<String> mGetContent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,4 +38,50 @@ public class MenuItemCategoryAddFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_menu_item_category_add, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //add photo button
+        initSelectPhoto(view);
+    }
+
+
+    //add photo button
+    private void initSelectPhoto(View v) {
+
+        //button
+        addPhotoButton = v.findViewById(R.id.menuItemCategoryAddImgButton);
+        addPhotoButton.setOnClickListener(this);
+        // photo
+        addImg = v.findViewById(R.id.menuItemCategoryAddImg);
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                        if(uri != null) addImg.setImageURI(uri);
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.menuItemCategoryAddImgButton:
+                chooseFile();
+                break;
+            case R.id.menuItemCategoryOkButton:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void chooseFile() {
+        mGetContent.launch("image/*");
+    }
+
 }
+

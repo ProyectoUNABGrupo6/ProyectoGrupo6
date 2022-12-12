a02519f4a08b7a03b8a19c1b4ae25c5aba3738bd
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-12 08:19:09 -0500

    dao_product product repository

---
+++ app/src/main/java/grupo6/proyectogrupo6/dao/ConvertersDao.java
@@ -0,0 +1,19 @@
+package grupo6.proyectogrupo6.dao;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class ConvertersDao {
+
+    @TypeConverter
+    public static Date fromTimestamp(Long value) {
+        return value == null ? null : new Date(value);
+    }
+
+    @TypeConverter
+    public static Long dateToTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+
+}
